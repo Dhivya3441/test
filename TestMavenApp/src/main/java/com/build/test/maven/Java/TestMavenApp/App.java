748cedd9d570c6a74681d646b5a656e2c207ac6b
ELCOT <ELCOT@Lenovo>
2023-04-03 10:58:45 +0530

    first commit

---
+++ TestMavenApp/src/main/java/com/build/test/maven/Java/TestMavenApp/App.java
@@ -0,0 +1,13 @@
+package com.build.test.maven.Java.TestMavenApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
